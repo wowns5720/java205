77e270eac548d566408e45474d3aa3c57d7c9534
wowns5720 <wowns5720@naver.com>
2021-05-24 18:26:23 +0900

    Update Member.java

--- java_project/MyProject/src/test/Member.java
+++ java_project/MyProject/src/test/Member.java
@@ -15,7 +15,10 @@ public Member(String name, int age, double height, boolean hasbook) {
 	public static void main(String[] args) {
 		
 		Member m = new Member("김재준", 20, 20.0, true);
-		System.out.println(m);
+		System.out.println(m.name);
+		System.out.println(m.age);
+		System.out.println(m.height);
+		System.out.println(m.hasbook);
 	}
 
 }
