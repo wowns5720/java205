4f485f63f87a4261642b7f7e01a0651c451ecc8c
wowns5720 <wowns5720@naver.com>
2021-05-25 18:19:23 +0900

    num 05
    num 05

--- java_project/MyProject/src/ch04/Member2.java
+++ java_project/MyProject/src/ch04/Member2.java
@@ -2,18 +2,49 @@ package ch04;
 
 public class Member2 {
 
-	String name;
-	int phoneNum;
-	String grade;
-	String email;
-	int birthDay;
-	String address;
+	private String name;
+	private int phoneNum;
+	private String grade;
+	private	String email;
+	private int birthDay;
+	private String address;
 	
-	void print() {
-		System.out.println(name+ " " + phoneNum+ " " + grade+ " " + email 
-				+ " " + birthDay+ " "  + address);
+	public String getName() {
+		return name;
 	}
-	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPhoneNum() {
+		return phoneNum;
+	}
+	public void setPhoneNum(int phoneNum) {
+		this.phoneNum = phoneNum;
+	}
+	public String getGrade() {
+		return grade;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getBirthDay() {
+		return birthDay;
+	}
+	public void setBirthDay(int birthDay) {
+		this.birthDay = birthDay;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
 	public Member2 (String name,int phoneNum,String grade,String email,int birthDay,String address){
 		this.name = name;
 		this.phoneNum = phoneNum;
@@ -28,13 +59,23 @@ public class Member2 {
 		this.grade = grade;
 		this.email = email;
 	}
+	public void printInfo() {
+		System.out.println("이름  : " + name);
+		System.out.println("번호  : " + phoneNum);
+		System.out.println("학년  : " + grade);
+		System.out.println("메일  : " + email);
+		System.out.println("생일  : " + birthDay);
+		System.out.println("주소  : " + address);
+	}
 	
 	public static void main(String[] args) {
 
-		Member2 m = new Member2("손흥민",123,"3학년","email",950407,"address");
-		m.print();
+		Member2 m = new Member2("손흥민",123,"3학년","email",920708,"토트넘");
+		m.printInfo();
+		System.out.println();
+		
 		Member2 m2 = new Member2("박지성",456,"4학년","email");
-		m2.print();
+		m2.printInfo();
 	}
 
 }
