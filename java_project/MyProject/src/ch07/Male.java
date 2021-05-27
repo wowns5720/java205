f3799fff728434c22269d983c81e0942b4228cfb
wowns5720 <wowns5720@naver.com>
2021-05-27 17:12:42 +0900

    상속
    상속

--- java_project/MyProject/src/ch07/Male.java
+++ java_project/MyProject/src/ch07/Male.java
@@ -2,11 +2,28 @@ package ch07;
 
 public class Male extends Person {
 
-	public Male(String name, String secNum, int age, float height) {
+	private String bed = "침대";
+	public String getBed() {
+		return bed;
+	}
+	public void setBed(String bed) {
+		this.bed = bed;
+	}
+	public Male(String name, String secNum, int age,String bed) {
 		super(name, secNum, age);
+		
 	}
 	void greet() {
 		System.out.println("안녕하세요. 저는 남자입니다.");
 	}
+	void rest() {
+		System.out.println("저는"+bed +"에서 쉬고 있습니다.");
+	}
+	public static void main(String[] args) {
+		Male m = new Male("남자","1999",23,"침대");
+		m.greet();
+		m.rest();
+	}
 	
 }
+
