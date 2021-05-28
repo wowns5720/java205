4eb9cdcfaaec7af4604f4256f4219be2d720985d
wowns5720 <wowns5720@naver.com>
2021-05-28 20:54:17 +0900

    0528
    0528

--- java_project/MyProject/src/ex/Product.java
+++ java_project/MyProject/src/ex/Product.java
@@ -5,9 +5,9 @@ public class Product {
 	final int price;
 	int bonusPoint;
 	
-	
-	public static void main(String[] args) {
-
+	Product(int price){
+		this.price = price;
+		bonusPoint = (int)(price/10.0);
 	}
-
+	
 }
