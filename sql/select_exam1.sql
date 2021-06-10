5e064e9d85fc1c6b90e5fd6a8b00d80259ccff1e
wowns5720 <wowns5720@naver.com>
2021-06-10 16:03:45 +0900

    Update select_exam1.sql

--- sql/select_exam1.sql
+++ sql/select_exam1.sql
@@ -69,7 +69,7 @@ from emp
 where ename like '%A%' and ename like '%E%'
 ;
 --14. 담당업무가 CLERK, 또는 SALESMAN이면서 급여가 $1600, $950 또는 $1300이 아닌 사원의 이름, 담당업무, 급여를 출력하시오.
-select ename,job,sal
+select ename, job, sal
 from emp
 where (job = 'CLERK' or job = 'salesman') and not (sal = 1600 or sal = 950 or sal = 1300)
 ;
