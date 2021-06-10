38ff0c1e0151ff865cf9d16523e4196204675d8d
wowns5720 <wowns5720@naver.com>
2021-06-10 15:14:03 +0900

    수정
    수정

--- sql/select_exam1.sql
+++ sql/select_exam1.sql
@@ -2,7 +2,7 @@
 --1. 덧셈연산자를 이용하여 모든 사원에 대해서 $300의 급여 인상을 계산한 후 사원의 이름, 급여, 인상된 급여를 출력하시오.
 select ename, sal, sal+300 as upsal
 from emp
-where sal >= 0 or sal<= 5000
+--where sal >= 0 or sal<= 5000
 ;
 --2. 사원의 이름, 급여, 연간 총 수입을 총 수입이 많은 것부터 작은 순으로 출력하시오, 연간 총수입은 월급에 12를 곱한 후 $100의 상여금을 더해서 계산하시오.
 select ename, sal, sal*12+100 as upsal
@@ -34,7 +34,7 @@ where hiredate between '81/02/20' and '81/05/01'
 select ename,deptno
 from emp
 where deptno between 20 and 30
-order by ename
+order by ename desc
 ;
 --8. 사원의 급여가 2000에서 3000사이에 포함되고 부서번호가 20 또는 30인 사원의 이름, 급여와 부서번호를 출력, 이름순(오름차순)으로 출력하시오.
 select ename, deptno, sal
@@ -50,7 +50,7 @@ where hiredate like '81%'
 --10. 관리자가 없는 사원의 이름과 담당 업무를 출력하시오.
 select ename, job
 from emp
-where job not like 'MANAGER'
+where mgr is null
 ;
 --11. 커미션을 받을 수 있는 자격이 되는 사원의 이름, 급여, 커미션을 출력하되 급여 및 커미션을 기준으로 내림차순 정렬하여 표시하시오.
 select ename, sal, comm
